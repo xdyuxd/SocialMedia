7dd04002535f7b89a87835ce50c7418797323751
xdyuxd <yyurickk@hotmail.com>
2019-06-21 17:54:51 -0300

    Created the models

---
+++ SocialMediaProject/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SocialMedia.Models
+{
+    public class Person
+    {
+        private string fullname;
+
+        public string nickname { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public DateTime birthdate { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+        //Person ()
+        //{
+        //    setFullname();
+        //}
+
+        public void setFullname()
+        {
+            fullname = name + ' ' + surname; 
+        }
+    }
+}
---
+++ SocialMediaProject/Models/Shout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialMedia.Models
+{
+    public class Shout
+    {
+        public Client owner { get; set; }
+        public string text { get; set; }
+
+        public string timestamp { get; set; }
+
+        public void delete()
+        {
+            //remove yourself.
+        }
+    }
+}
