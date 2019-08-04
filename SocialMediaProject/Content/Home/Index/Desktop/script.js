0011a14bfac1fac6dd8321ccc15e2293afd5d49e
xdyuxd <yyurickk@hotmail.com>
2019-08-04 14:11:23 -0300

    Login and registration
    Creating login and registrating scrips with AJAX as well creating animations and layout for using with both of them

--- SocialMediaProject/Content/Home/Index/Desktop/script.js
+++ SocialMediaProject/Content/Home/Index/Desktop/script.js
@@ -1,1 +1,44 @@
-﻿
+﻿$(document).ready(function (e) {
+    function runEffect() {
+        $("#box_registration").fadeOut(1);
+        $("#box_login").fadeIn(450);
+    }
+
+    $("#login_button").on("click", function () {
+            runEffect();
+            return false;
+    });
+    $("#button_return").on("click", function () {
+        $("#box_login").fadeOut(1);
+        $("#box_registration").fadeIn(450);
+        return false;
+    });
+
+    function Login() {
+        $.ajax
+            ({
+                type: "POST",
+                url: "https://localhost:44331/api/client/create",
+                crossDomain: true,
+                dataType: 'json',
+                async: true,
+                headers: {
+                    'nickname': $("input#nickname").val(),
+                    'name': $("input#first_name").val(),
+                    'surname': $("input#last_name").val(),
+                    'email': $("input#email").val(),
+                    'birthdate': new Date($("input#birthdate").val()).getTime() / 1000,
+                    'password': $("input#password").val(),
+
+                }
+            });
+    }
+
+
+    $("#register_button").on("click", function () {
+        Login();
+        return false;
+    });
+});
+
+
