0a196744ba4e370dbe1d0d30fce28e566dee4cb8
xdyuxd <yyurickk@hotmail.com>
2019-08-14 00:08:02 -0300

    Editing Profile Page
    Editing elements on the profile page as BIO and PICTURE

--- SocialMediaProject/Content/Profile/Index/Desktop/script.js
+++ SocialMediaProject/Content/Profile/Index/Desktop/script.js
@@ -1,1 +1,4 @@
-﻿
+﻿function edit-bio() {
+    document.getElementById("bio-text").contentEditable = true;
+    document.getElementById("demo").innerHTML = "Você agora pode digitar sua nova biografia.";
+}
