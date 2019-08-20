7737244a4bbfa318424d883a5f57188a04e1490d
Shadow <shadowvonwolfkin@gmail.com>
2019-08-19 23:10:34 -0300

    Redesign the form layout in the homepage

--- SocialMediaProject/Content/Home/Index/Desktop/script.js
+++ SocialMediaProject/Content/Home/Index/Desktop/script.js
@@ -1,24 +1,19 @@
 ﻿$(document).ready(function (e) {
+
+    //Date
+    var now = new Date();
+    var day = ("0" + now.getDate()).slice(-2);
+    var month = ("0" + (now.getMonth() + 1)).slice(-2);
+    var today = now.getFullYear() + "-" + (month) + "-" + (day);
+    $("#birthdate").val(today);
+
+
     function runEffect() {
-        $("#box-registration").fadeOut(1);
-        $("#box_login").fadeIn(450);
+        $("#box-registration").fadeOut(1)
+        $("#box_login").fadeIn(450)
     }
 
-    $("#login_button").on("click", function () {
-            runEffect();
-            return false;
-    });
-    $("#button_return").on("click", function () {
-        $("#box_login").fadeOut(1);
-        $("#box-registration").fadeIn(450);
-        return false;
-    });
-    $("#register_button").on("click", function () {
-        Login();
-        return false;
-    });
-
-    function Login() {
+    function Register() {
         $.ajax
             ({
                 type: "POST",
@@ -38,17 +33,39 @@
             });
     }
 
-    var first_color = new Array(199, 179, 255, 100);
-    var second_color = new Array(212, 208, 162, 83); //https://www.gradient-animator.com/ 
-    setInterval(function () {
-        $(".gradient").css({ "background-image": `linear-gradient(${deg}deg, rgba(${first_color.toString()}), rgba(${second_color.toString()})` })
-        console.log(first_color.toString())
 
+<<<<<<< HEAD
+    $(".register-form-login").on("click", function () {
+            runEffect();
+    });
+
+    $("#button_return").on("click", function () {
+        $("#box_login").fadeOut(1);
+        $("#box-registration").fadeIn(450);
+    });
+
+    $(".register-form-sign-up").on("click", function () {
+        Register();
+    });
+
+
+
+    //var first_color = new Array(199, 179, 255, 100);
+    //var second_color = new Array(212, 208, 162, 83); //https://www.gradient-animator.com/ 
+    //setInterval(function () {
+    //    $(".gradient").css({ "background-image": `linear-gradient(${deg}deg, rgba(${first_color.toString()}), rgba(${second_color.toString()})` })
+    //    console.log(first_color.toString())
+
+    //    //first_color[0] += 15;
+    //    //second_color[0] -= 15;
+    //    deg += 15;
+=======
         first_color[0] += 15;
         second_color[0] -= 15;
         deg += 15;
+>>>>>>> 879a93c323404c6d10eda472b6fbed4e33e9a579
 
-    }, 1500);
+    //}, 1500);
 
 
 
