feee6e234865b6a493fc02e041e932f896c23d12
Shadow <shadowvonwolfkin@gmail.com>
2019-09-03 21:10:34 -0300

    New layout for the Homepage.

--- SocialMediaProject/Content/Home/Index/Desktop/script.js
+++ SocialMediaProject/Content/Home/Index/Desktop/script.js
@@ -5,50 +5,17 @@
     var day = ("0" + now.getDate()).slice(-2);
     var month = ("0" + (now.getMonth() + 1)).slice(-2);
     var today = now.getFullYear() + "-" + (month) + "-" + (day);
-    $("#birthdate").val(today);
+    $('input[name="birthdate"]').val(today);
 
-
-    function runEffect() {
-        $("#box-registration").fadeOut(1)
-        $("#box_login").fadeIn(450)
-    }
-
-    function Register() {
-        $.ajax
-            ({
-                type: "POST",
-                url: "https://localhost:44331/api/client/create",
-                crossDomain: true,
-                dataType: 'json',
-                async: true,
-                headers: {
-                    'nickname': $("input#nickname").val(),
-                    'name': $("input#first_name").val(),
-                    'surname': $("input#last_name").val(),
-                    'email': $("input#email").val(),
-                    'birthdate': new Date($("input#birthdate").val()).getTime() / 1000,
-                    'password': $("input#password").val(),
-
-                }
-            });
-    }
-
-
-<<<<<<< HEAD
-    $(".register-form-login").on("click", function () {
-            runEffect();
+    $(".register-button-login").on("click", function () {
+        $(".box-registration").fadeOut(1)
+        $(".box-login").fadeIn(450)
     });
 
-    $("#button_return").on("click", function () {
-        $("#box_login").fadeOut(1);
-        $("#box-registration").fadeIn(450);
+    $(".login-button-back").on("click", function () {
+        $(".box-login").fadeOut(1);
+        $(".box-registration").fadeIn(450);
     });
-
-    $(".register-form-sign-up").on("click", function () {
-        Register();
-    });
-
-
 
     //var first_color = new Array(199, 179, 255, 100);
     //var second_color = new Array(212, 208, 162, 83); //https://www.gradient-animator.com/ 
@@ -59,15 +26,8 @@
     //    //first_color[0] += 15;
     //    //second_color[0] -= 15;
     //    deg += 15;
-=======
-        first_color[0] += 15;
-        second_color[0] -= 15;
-        deg += 15;
->>>>>>> 879a93c323404c6d10eda472b6fbed4e33e9a579
 
     //}, 1500);
-
-
 
 });
 
