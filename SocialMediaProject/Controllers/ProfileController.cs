b549c44a971c091a9569a787e1aa51b4e9d45fd2
Shadow <shadowvonwolfkin@gmail.com>
2019-09-17 21:19:56 -0300

    Added the profile error method

--- SocialMediaProject/Controllers/ProfileController.cs
+++ SocialMediaProject/Controllers/ProfileController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,19 +11,38 @@ namespace SocialMedia.Controllers
 {
     public class ProfileController : Controller
     {
+        private string url = "https://localhost:44331/api";
+
         [HttpGet, OutputCache(Duration = 60 * 15, VaryByParam = "none")]
-        public ActionResult Index(string nickname)
+        public async Task<ActionResult> Index(string nickname)
         {
-            ViewBag.Name = "Name";
+            ViewBag.Name = "Name - Profile";
             ViewBag.Bio = "Escreva aqui sua biografia, um pouco de fatos particulares de sua vida.";
             if ((string)Session["Client"] == nickname)
             {
                 Dictionary<string, string> client = (Dictionary<string, string>)Session["ClientCollection"];
-                ViewBag.Name = client["Name"];
+                ViewBag.Name = $"client['Name'] - Profile";
             }
             else
             {
-
+                try
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+                        HttpResponseMessage response = await client.GetAsync($"{url}/client/{nickname}");
+                        response.EnsureSuccessStatusCode();
+                        string r = await response.Content.ReadAsStringAsync();
+                        if (r.Contains("Not Found."))
+                        {
+                            return RedirectToAction("Error");
+                        }
+                        var dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(r);
+                    }
+                }
+                catch
+                {
+                    return RedirectToAction("Error");
+                }
             }
             return View();
         }
@@ -36,6 +58,13 @@ namespace SocialMedia.Controllers
         {
             Session["Client"] = null;
             return Json(new { url = Url.Action("Index", "Home") });
+        }
+
+        [HttpGet]
+        public ActionResult Error()
+        {
+            ViewBag.Name = "Not found - SocialMedia";
+            return View();
         }
     }
 }
